8e946137d154b40da44dd083fddc61d1a7e7596a
nuttawit007 <nuttawit.ph@gmail.com>
2025-10-01 20:19:37 +0700

    fix(model) : change base class model

--- client/models.py
+++ client/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
+class User(models.Model):
     class Role(models.Choices):
         CUSTOMER = "CUSTOMER"
         STAFF = "STAFF"
